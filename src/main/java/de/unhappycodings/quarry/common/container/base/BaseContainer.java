87d2f5e57ef32da6c3914facdeeb981f2fb38c5c
UnhappyCodings <gannatic.german@gmail.com>
2023-04-30 18:27:36 +0200

    + Fixed illegal forward reference

--- src/main/java/de/unhappycodings/quarry/common/container/base/BaseContainer.java
+++ src/main/java/de/unhappycodings/quarry/common/container/base/BaseContainer.java
@@ -27,9 +27,9 @@ public abstract class BaseContainer extends AbstractContainerMenu {
     private static final int PLAYER_INVENTORY_COLUMN_COUNT = 9;
     private static final int PLAYER_INVENTORY_SLOT_COUNT = PLAYER_INVENTORY_COLUMN_COUNT * PLAYER_INVENTORY_ROW_COUNT;
     private static final int VANILLA_SLOT_COUNT = HOTBAR_SLOT_COUNT + PLAYER_INVENTORY_SLOT_COUNT;
-    private static final int TE_INVENTORY_FIRST_SLOT_INDEX = VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT;
     private static final int VANILLA_FIRST_SLOT_INDEX = 0;
     private static final int TE_INVENTORY_SLOT_COUNT = 14;
+    private static final int TE_INVENTORY_FIRST_SLOT_INDEX = VANILLA_FIRST_SLOT_INDEX + VANILLA_SLOT_COUNT;
     private final IItemHandler inventory;
     protected QuarryBlockEntity tileEntity;
 
