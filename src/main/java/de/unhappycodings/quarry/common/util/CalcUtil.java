4220f129ba91dbf13c85265c70d88abcf9a249c0
UnhappyCodings <gannatic.german@gmail.com>
2026-05-20 11:45:00 +0200

    + Reworked getBlockCount() as it returned wrong values

--- src/main/java/de/unhappycodings/quarry/common/util/CalcUtil.java
+++ src/main/java/de/unhappycodings/quarry/common/util/CalcUtil.java
@@ -52,6 +52,49 @@ public class CalcUtil {
         return blocks;
     }
 
+    public static int getBlockAmount(BlockPos loc1, BlockPos loc2) {
+        int i = 0;
+        int x1 = loc1.getX();
+        int y1 = loc1.getY();
+        int z1 = loc1.getZ();
+        int x2 = loc2.getX();
+        int y2 = loc2.getY();
+        int z2 = loc2.getZ();
+        int xMin, yMin, zMin;
+        int xMax, yMax, zMax;
+        int x, y, z;
+        if (x1 > x2) {
+            xMin = x2;
+            xMax = x1;
+        } else {
+            xMin = x1;
+            xMax = x2;
+        }
+        if (y1 > y2) {
+            yMin = y2;
+            yMax = y1;
+        } else {
+            yMin = y1;
+            yMax = y2;
+        }
+        if (z1 > z2) {
+            zMin = z2;
+            zMax = z1;
+        } else {
+            zMin = z1;
+            zMax = z2;
+        }
+
+        for (y = yMax; y >= yMin; y--) {
+            for (x = xMin; x <= xMax; x++) {
+                for (z = zMin; z <= zMax; z++) {
+                    i++;
+                }
+            }
+        }
+        return i;
+    }
+
     public static int getBlockCount(BlockPos pos1, BlockPos pos2) {
         int x1 = pos1.getX();
         int y1 = pos1.getY();
